fbd0d110d11e1c13eeaae299d86e737cbdcee08d
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-12-09 19:54:38 +0200

    create my app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import Profile from './components/Profile';
 import './App.css';
+import user from './data/user.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Profile
+      username={user.username}
+      avatar={user.avatar}
+      tag={user.tag}
+      location={user.location}
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes}
+    />
   );
 }
 
